00e4e2a98ac45b7cc73774e63c8527c8e7d1e74c
Muaz <muaz.syed603@gmail.com>
2026-09-01 14:32:03 +0530

    Time: 3 ms (18.44%), Space: 19.4 MB (42.05%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-concatenation-of-array/0001-concatenation-of-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getConcatenation(self, nums):
+        ans = []
+
+        for x in nums:
+            ans.append(x)
+
+        for x in nums:
+            ans.append(x)
+
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
